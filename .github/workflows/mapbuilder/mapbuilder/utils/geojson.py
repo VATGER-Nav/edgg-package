956bc025a983e8b0eea8935c0073bb815755a0ff
Sergej Singer <74149595+sergej-singer@users.noreply.github.com>
2026-08-07 02:15:21 +0200

    Update Mapbuilder to v2

--- .github/workflows/mapbuilder/mapbuilder/utils/geojson.py
+++
@@ -1,6 +0,0 @@
-from shapely.io import from_geojson
-
-
-def load_geojson(filename):
-    with open(filename, "rb") as f:
-        return from_geojson(f.read())
--- .github/workflows/mapbuilder/mapbuilder/utils/json.py
+++
@@ -1,6 +0,0 @@
-from json import load
-
-
-def load_json(filename):
-    with open(filename, "r") as f:
-        return load(f)
